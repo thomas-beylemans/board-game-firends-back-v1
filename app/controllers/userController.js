8b17fa21fa84becd7be7add23b638f07c551f3ef
Thomas BEYLEMANS <thomas.beylemans@gmail.com>
2022-05-09 16:05:29 +0200

    adding console.log to track the problem on Heroku

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -5,10 +5,13 @@ const userController = {
     async register(req, res, next){
         try {
             const result = await dataMapper.addOneUser(req.body);
+            console.log(result);
             
             const accessToken = createAccessToken(result);
             
             // push the accessToken in req in case of next middleware
+            console.body('accessToken:');
+            console.log(accessToken);
             req.userToken = accessToken;
             // TODO: précicer le contenu de la réponse à apporter au front
             res.status(201).json({
@@ -30,7 +33,11 @@ const userController = {
             // delete isAuthorized from result to avoid sending it to the client
             delete result.isAuthorized;
             
+            console.body('req.body:');
+            console.log(req.body);
             const accessToken = createAccessToken(req.body);
+            console.body('accessToken:');
+            console.log(accessToken);
             //?
             res.status(200).json({username: result.username, accessToken});
             //?
