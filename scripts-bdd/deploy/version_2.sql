302821f1d1bfb6ec0a4c0b502fbc7e39935fe905
sakomoto987 <brandon.sacault@gmail.com>
2022-05-10 16:25:22 +0200

    image added to user database

--- scripts-bdd/deploy/version_2.sql
+++ scripts-bdd/deploy/version_2.sql
@@ -12,9 +12,9 @@ INSERT INTO public.user(
     "email", "password", "username", "avatar", "bio", "geo_id"  )
     VALUES
     ('tom@oclock.com', 'toto', 'Tom', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652126548/images/306452.jpg', 'dev junior', 1),
-    ('tommy@oclock.com', 'toto', 'Tommy', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652126548/images/306452.jpg', 'dev junior', 1),
-    ('tommas@oclock.com', 'toto', 'Tommas', 'dev junior', 2),
-    ('toas@oclock.com', 'toto', 'Toas', 'dev junior', 1);
+    ('tommy@oclock.com', 'toto', 'Tommy', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652192466/images/logo-franchise-tommy-hilfiger_ziikbn.png', 'dev junior', 1),
+    ('tommas@oclock.com', 'toto', 'Tommas', null, 'dev junior', 2),
+    ('toas@oclock.com', 'toto', 'Toas', null, 'dev junior', 1);
 
 
 COMMIT;
