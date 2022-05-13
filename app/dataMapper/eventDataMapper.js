9587b949c7ec93e4ed640771b09a5d6f09a02e5d
Thomas BEYLEMANS <thomas.beylemans@gmail.com>
2022-05-13 17:23:31 +0200

    route GET events with zoom factor

--- app/dataMapper/eventDataMapper.js
+++ app/dataMapper/eventDataMapper.js
@@ -1,76 +1,63 @@
-const { status } = require('express/lib/response');
 const pool = require('../database/dbClient');
 const toolsDataMapper = require('./toolsDataMapper')
 
 const eventDataMapper = {
-    async getEvents(){
-        const results = {
-            "rows": {
-                "event": [
-                    {
-                        "id": 100,
-                        "name": "Event 100",
-                        "picture": 'http://www.spainisculture.com/export/sites/cultura/multimedia/galerias/fiestas/fiestas_sin_ficha/ajedrez2_javea_o_c.jpg_1306973099.jpg',
-                        "seats": 10,
-                        "description": "Je ne joue pas pour participer, je joue pour que vous sachiez que je suis le meilleur !",
-                        "start_date": "2022-05-04T18:00:00.000Z",
-                        "event_admin": {
-                            "id": 10,
-                            "username": "Chat10",
-                            "avatar": "https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg"
-                        },
-                        "geo": {
-                            "id": 17,
-                            "city": "Lattes",
-                            "postcode": 34970,
-                            "lat": 43.567,
-                            "long": 3.899
-                        }
-                    },
-                    {
-                        "id": 27,
-                        "name": "Event 27",
-                        "picture": 'https://d2lv662meabn0u.cloudfront.net/boomerang/dynamic/editorial/00000000/421/1665670a7d1dc8574f071cb6aaeef85deb66c291_1589991670.webp',
-                        "seats": 7,
-                        "description": "Super soirée où ça sera fun mais on ne sait pas quoi faire pour l'instant...",
-                        "start_date": "2022-05-25T19:00:00.000Z",
-                        "event_admin": {
-                            "id": 71,
-                            "username": "Chat71",
-                            "avatar": "https://cdn.pixabay.com/photo/2012/02/27/16/57/cat-17430_960_720.jpg"
-                        },
-                        "geo": {
-                            "id": 21,
-                            "city": "Villeneuve-lès-Maguelone",
-                            "postcode": 34750,
-                            "lat": 43.5119,
-                            "long": 3.8589
-                        }
-                    },
-                    {
-                        "id": 589,
-                        "name": "Event trop trop cool",
-                        "picture": 'https://cache.magazine-avantages.fr/data/photo/w1000_ci/1jv/cartes-a-jouer-diy-do-it-yourself.jpg',
-                        "seats": 31,
-                        "description": "Une aprèm entière à faire des solitaires entre amis, ça te dit ?!",
-                        "start_date": "2022-05-27T12:00:00.000Z",
-                        "event_admin": {
-                            "id": 4,
-                            "username": "GriffeurDu34",
-                            "avatar": "https://cdn.pixabay.com/photo/2015/01/04/10/46/lion-588144_960_720.jpg"
-                        },
-                        "geo": {
-                            "id": 3971,
-                            "city": "Palavas-les-Flots",
-                            "postcode": 34250,
-                            "lat": 43.5323,
-                            "long": 3.9346
-                        }
-                    }
-                ]
-            }
+    async getEvents(userProfile, zoomFactor = 1){
+        const query = {
+            text: `SELECT
+                "event"."id",
+                "event"."name",
+                "event"."picture",
+                "event"."seats",
+                "event"."start_date",
+                "event"."description",
+                "event"."created_at",
+                "geo"."id" AS "geo_id",
+                "geo"."city" AS "city",
+                "geo"."postcode" AS "postcode",
+                "geo"."lat" AS "lat",
+                "geo"."long" AS "long",
+                "user"."id" AS "event_admin_id",
+                "user"."username" AS "event_admin_username",
+                "user"."avatar" AS "event_admin_avatar"
+            FROM
+                "geo"
+            INNER JOIN "event" ON ("geo"."id" = "event"."geo_id")
+            INNER JOIN "user" ON ("event"."event_admin" = "user"."id")
+            WHERE
+                "lat" <= $1::float + $2::float
+                AND "long" <= $3::float + $2::float
+                AND "lat" >= $1::float - $2::float
+                AND "long" >= $3::float - $2::float`,
+            values: [userProfile.geo.lat, (zoomFactor/10), userProfile.geo.long]
         }
-        return results.rows;
+        const result = await pool.query(query);
+        if(result.rowCount === 0){
+            return { message: `Il n'y a pas d'événement dans cette zone de recherche.`, zoomFactor: zoomFactor }
+        }
+        const resultToReturn = result.rows.map(event => {
+            return {
+                id: event.id,
+                name: event.name,
+                picture: event.picture,
+                seats: event.seats,
+                start_date: event.start_date,
+                description: event.description,
+                event_admin: {
+                    id: event.event_admin_id,
+                    username: event.event_admin_username,
+                    avatar: event.event_admin_avatar
+                },
+                geo: {
+                    id: event.geo_id,
+                    city: event.city,
+                    postcode: event.postcode,
+                    lat: event.lat,
+                    long: event.long
+                }
+            }
+        });
+        return resultToReturn;
     },
     async getEventById(id){
         const results = {
