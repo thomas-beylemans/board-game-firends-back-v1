be736a426b077cdfd5b660d06bb863bbd3576724
Thomas BEYLEMANS <thomas.beylemans@gmail.com>
2022-05-19 09:51:06 +0200

    database empty after generation with sqitch

--- scripts-bdd/deploy/version_2.sql
+++ scripts-bdd/deploy/version_2.sql
@@ -2,40 +2,40 @@
 
 BEGIN;
 
-INSERT INTO public.geo(
-    "city", "postcode", "lat", "long" )
-    VALUES
-    ('Paris', 75000, 48.8589, 2.347),
-    ('Montpellier', 34000, 43.61, 3.8742),
-    ('Lyon', 69000, 45.76, 4.84),
-    ('Marseille', 13000, 43.29, 5.36),
-    ('Toulouse', 31000, 43.60, 1.44),
-    ('Lattes', 34970, 43.567, 3.899),
-    ('Villeneuve-lès-Maguelone', 34750, 43.5119, 3.8589),
-    ('Palavas-les-Flots', 34250, 43.5323, 3.9346),
-    ('Saint-Paul-et-Valmalle', 34570, 43.6152, 4.6843),
-    ('Sète', 34200, 43.3841, 3.644),
-    ('Bordeaux', 33000, 44.83, -0.57),
-    ('Nantes', 44000, 47.21, -1.55),
-    ('Le Havre', 70000, 49.49, 0.1),
-    ('Rennes', 35000, 48.08, -1.67),
-    ('Brest', 29000, 48.42, -4.48),
-    ('Le Mans', 50000, 48.00, 0.00),
-    ('Dijon', 21000, 47.32, 5.04),
-    ('Ajaccio', 20000, 41.91, 8.5),
-    ('Cannes', 20000, 43.55, 7.01);
+-- INSERT INTO public.geo(
+--     "city", "postcode", "lat", "long" )
+--     VALUES
+--     ('Paris', 75000, 48.8589, 2.347),
+--     ('Montpellier', 34000, 43.61, 3.8742),
+--     ('Lyon', 69000, 45.76, 4.84),
+--     ('Marseille', 13000, 43.29, 5.36),
+--     ('Toulouse', 31000, 43.60, 1.44),
+--     ('Lattes', 34970, 43.567, 3.899),
+--     ('Villeneuve-lès-Maguelone', 34750, 43.5119, 3.8589),
+--     ('Palavas-les-Flots', 34250, 43.5323, 3.9346),
+--     ('Saint-Paul-et-Valmalle', 34570, 43.6152, 4.6843),
+--     ('Sète', 34200, 43.3841, 3.644),
+--     ('Bordeaux', 33000, 44.83, -0.57),
+--     ('Nantes', 44000, 47.21, -1.55),
+--     ('Le Havre', 70000, 49.49, 0.1),
+--     ('Rennes', 35000, 48.08, -1.67),
+--     ('Brest', 29000, 48.42, -4.48),
+--     ('Le Mans', 50000, 48.00, 0.00),
+--     ('Dijon', 21000, 47.32, 5.04),
+--     ('Ajaccio', 20000, 41.91, 8.5),
+--     ('Cannes', 20000, 43.55, 7.01);
 
 
-INSERT INTO public.user(
-    "email", "password", "username", "avatar", "bio", "geo_id"  )
-    VALUES
-    ('tom@oclock.com', 'toto', 'Tom', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652196952/images/kisspng-tom-cat-kitten-tom-and-jerry-talking-tom-and-frien-tom-and-jerry-5ac7adfeda7190.7836761415230356468948_lhogpv.jpg', 'dev junior', 2),
-    ('tommy@oclock.com', 'toto', 'Tommy', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652192466/images/logo-franchise-tommy-hilfiger_ziikbn.png', 'dev junior', 1),
-    ('tommas@oclock.com', 'toto', 'Tommas', null, 'dev junior', 2),
-    ('toas@oclock.com', 'toto', 'Toas', null, 'dev junior', 1),
-    ('Chat10@oclock.com', 'toto', 'Chat10', 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg', 'Description de Chat10', 5),
-    ('Chat71@oclock.com', 'toto', 'Chat71', 'https://cdn.pixabay.com/photo/2012/02/27/16/57/cat-17430_960_720.jpg', 'Description de Chat71', 4),
-    ('GriffeurDu34@oclock.com', 'toto', 'GriffeurDu34', 'https://cdn.pixabay.com/photo/2015/01/04/10/46/lion-588144_960_720.jpg', 'Description de GriffeurDu34', 2);    
+-- INSERT INTO public.user(
+--     "email", "password", "username", "avatar", "bio", "geo_id"  )
+--     VALUES
+--     ('tom@oclock.com', 'toto', 'Tom', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652196952/images/kisspng-tom-cat-kitten-tom-and-jerry-talking-tom-and-frien-tom-and-jerry-5ac7adfeda7190.7836761415230356468948_lhogpv.jpg', 'dev junior', 2),
+--     ('tommy@oclock.com', 'toto', 'Tommy', 'https://res.cloudinary.com/board-game-friends/image/upload/v1652192466/images/logo-franchise-tommy-hilfiger_ziikbn.png', 'dev junior', 1),
+--     ('tommas@oclock.com', 'toto', 'Tommas', null, 'dev junior', 2),
+--     ('toas@oclock.com', 'toto', 'Toas', null, 'dev junior', 1),
+--     ('Chat10@oclock.com', 'toto', 'Chat10', 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg', 'Description de Chat10', 5),
+--     ('Chat71@oclock.com', 'toto', 'Chat71', 'https://cdn.pixabay.com/photo/2012/02/27/16/57/cat-17430_960_720.jpg', 'Description de Chat71', 4),
+--     ('GriffeurDu34@oclock.com', 'toto', 'GriffeurDu34', 'https://cdn.pixabay.com/photo/2015/01/04/10/46/lion-588144_960_720.jpg', 'Description de GriffeurDu34', 2);    
 
 
 COMMIT;
--- scripts-bdd/deploy/version_4.sql
+++ scripts-bdd/deploy/version_4.sql
@@ -3,21 +3,21 @@
 
 BEGIN;
 
-INSERT INTO public.event(
-    "name", "seats", "start_date", "description", "event_admin", "geo_id" ) 
-    VALUES
-    ('Event 100', 7, '2022-05-04T18:00:00.000Z', 'Je ne joue pas pour participer, je joue pour que vous sachiez que je suis le meilleur !', 7, 6),
-    ('Event 27', 4, '2022-05-25T19:00:00.000Z', 'Super soirée où ça sera fun mais on ne sait pas quoi faire pour l\''instant...', 6, 7),
-    ('Event trop trop cool', 2, '2022-05-27T12:00:00.000Z', 'Une aprèm entière à faire des solitaires entre amis, ça te dit ?!', 7, 8),
-    ('Event pas si mal', 2, '2022-05-26T12:00:00.000Z', 'B;ab;;laskdg;lkasdg;lk; ladjg', 7, 9),
-    ('Grosse teuf', 2, '2022-05-29T12:00:00.000Z', 'mnvkjfsdnvkgjrrhg', 7, 10);
+-- INSERT INTO public.event(
+--     "name", "seats", "start_date", "description", "event_admin", "geo_id" ) 
+--     VALUES
+--     ('Event 100', 7, '2022-05-04T18:00:00.000Z', 'Je ne joue pas pour participer, je joue pour que vous sachiez que je suis le meilleur !', 7, 6),
+--     ('Event 27', 4, '2022-05-25T19:00:00.000Z', 'Super soirée où ça sera fun mais on ne sait pas quoi faire pour l\''instant...', 6, 7),
+--     ('Event trop trop cool', 2, '2022-05-27T12:00:00.000Z', 'Une aprèm entière à faire des solitaires entre amis, ça te dit ?!', 7, 8),
+--     ('Event pas si mal', 2, '2022-05-26T12:00:00.000Z', 'B;ab;;laskdg;lkasdg;lk; ladjg', 7, 9),
+--     ('Grosse teuf', 2, '2022-05-29T12:00:00.000Z', 'mnvkjfsdnvkgjrrhg', 7, 10);
 
-INSERT INTO public.game(
-    "name", "picture"  )
-    VALUES
-    ('Monopoly', 'https://cdn3.philibertnet.com/375938-large_default/monopoly-classique.jpg'),
-    ('Mystic Vale', 'https://cdn3.philibertnet.com/358209-large_default/mystic-vale.jpg'),
-    ('Dice Throne Adventures', 'https://cdn1.philibertnet.com/536883-medium_default/dice-throne-adventures.jpg');
+-- INSERT INTO public.game(
+--     "name", "picture"  )
+--     VALUES
+--     ('Monopoly', 'https://cdn3.philibertnet.com/375938-large_default/monopoly-classique.jpg'),
+--     ('Mystic Vale', 'https://cdn3.philibertnet.com/358209-large_default/mystic-vale.jpg'),
+--     ('Dice Throne Adventures', 'https://cdn1.philibertnet.com/536883-medium_default/dice-throne-adventures.jpg');
 
 -- INSERT INTO public.chatroom(
 --     "name", "username", "message"  )
@@ -36,24 +36,24 @@ INSERT INTO public.game(
 --     (1, 1),
 --     (1, 2);
 
-INSERT INTO public.user_joins_event(
-    "user_id", "event_id" )
-    VALUES
-    (1, 1),
-    (1, 3),
-    (2, 1),
-    (3, 1),
-    (4, 1);
+-- INSERT INTO public.user_joins_event(
+--     "user_id", "event_id" )
+--     VALUES
+--     (1, 1),
+--     (1, 3),
+--     (2, 1),
+--     (3, 1),
+--     (4, 1);
 
-INSERT INTO public.user_owns_game(
-"user_id", "game_id" )
-VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 2),
-(3, 1),
-(4, 1);
+-- INSERT INTO public.user_owns_game(
+-- "user_id", "game_id" )
+-- VALUES
+-- (1, 1),
+-- (1, 2),
+-- (1, 3),
+-- (2, 2),
+-- (3, 1),
+-- (4, 1);
 
 COMMIT;
 
